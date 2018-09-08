1b4d593cd838d28d92511ed68a5ccff68a9c6c66
John Holland <hollandjb@mail.nih.gov>
2018-09-08 11:41:36 -0400

    revert unnneeded changes

--- src/main/java/com/github/searls/jasmine/server/ServerManager.java
+++ src/main/java/com/github/searls/jasmine/server/ServerManager.java
@@ -5,51 +5,44 @@ import org.eclipse.jetty.server.Server;
 
 public class ServerManager {
 
-	private static final int ANY_PORT = 0;
+  private static final int ANY_PORT = 0;
 
-	private final Server server;
-	private final Connector connector;
-	private final ResourceHandlerConfigurator configurator;
+  private final Server server;
+  private final Connector connector;
+  private final ResourceHandlerConfigurator configurator;
 
-	public ServerManager(Server server, Connector connector, ResourceHandlerConfigurator configurator) {
-		this.server = server;
-		this.connector = connector;
-		this.configurator = configurator;
-	}
+  public ServerManager(Server server,
+                       Connector connector,
+                       ResourceHandlerConfigurator configurator) {
+    this.server = server;
+    this.connector = connector;
+    this.configurator = configurator;
+  }
 
-	public int start() throws Exception {
-		if (System.getenv("JASMINE_COVERAGE_PORT") != null) {
-			try {
-				int port = Integer.valueOf(System.getenv("JASMINE_COVERAGE_PORT"));
-				return this.startServer(port);
-			} catch (Exception e) {
-				e.printStackTrace();
-				// fall through and use ANY_PORT
-			}
-		}
-		return this.startServer(ANY_PORT);
-	}
+  public int start() throws Exception {
+    return this.startServer(ANY_PORT);
+  }
 
-	public void start(int port) throws Exception {
-		this.startServer(port);
-	}
+  public void start(int port) throws Exception {
+    this.startServer(port);
+  }
 
-	private int startServer(int port) throws Exception {
-		connector.setPort(port);
+  private int startServer(int port) throws Exception {
+    connector.setPort(port);
 
-		this.server.setHandler(this.configurator.createHandler());
-		this.server.addConnector(connector);
+    this.server.setHandler(this.configurator.createHandler());
+    this.server.addConnector(connector);
 
-		this.server.start();
+    this.server.start();
 
-		return connector.getLocalPort();
-	}
+    return connector.getLocalPort();
+  }
 
-	public void stop() throws Exception {
-		this.server.stop();
-	}
+  public void stop() throws Exception {
+    this.server.stop();
+  }
 
-	public void join() throws Exception {
-		this.server.join();
-	}
+  public void join() throws Exception {
+    this.server.join();
+  }
 }
