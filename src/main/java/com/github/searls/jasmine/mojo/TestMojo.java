25fadf8a6047f8df909a0bfb8d2cb5a763a881b4
John Holland <hollandjb@mail.nih.gov>
2018-09-10 11:55:46 -0400

    fallback to random port behavior if using property fails

--- src/main/java/com/github/searls/jasmine/mojo/TestMojo.java
+++ src/main/java/com/github/searls/jasmine/mojo/TestMojo.java
@@ -193,6 +193,10 @@ public class TestMojo extends AbstractJasmineMojo {
 		int port = this.serverPort;
 		serverManager.start(this.serverPort);
        setPortProperty(port);
+    } catch (Throwable t){
+    	int port = serverManager.start();
+    	setPortProperty(port);
+    }
       this.getLog().info("Executing Jasmine Specs");
       JasmineResult result = this.executeSpecs(new URL(this.uriScheme + "://" + this.serverHostname + ":" + port));
       this.logResults(result);
